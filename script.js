515ab3de56606f24a21fef837a9f3a0a6e4f4c47
O-Midey <46810599+O-Midey@users.noreply.github.com>
2022-09-30 18:44:34 +0100

    completed the courses section

--- script.js
+++ script.js
@@ -2,3 +2,18 @@
 
 // Active navbar onclick
 // Showw/hide password
+
+var buttons = document.querySelectorAll(".filter-button");
+var newButton = document.getElementById("#new");
+var mostPopularButton = document.getElementById("#most-popular");
+
+newButton.addEventListener("click", changeDisplay);
+const removeClass = () => {
+    buttons.classList.remove("active");
+}
+
+
+const changeDisplay = () => {
+    buttons.forEach(removeClass);
+    newButton.classList.add("active")
+}
